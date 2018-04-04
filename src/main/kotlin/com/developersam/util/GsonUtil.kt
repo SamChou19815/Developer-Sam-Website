3ccf83b1439289ef9594f5d5dd34dbea9ec04c3e
Sam <sam@developersam.com>
2018-04-04 19:11:11 -0400

    Fix import

--- src/main/kotlin/com/developersam/util/GsonUtil.kt
+++ src/main/kotlin/com/developersam/util/GsonUtil.kt
@@ -2,6 +2,7 @@
 
 package com.developersam.util
 
+import com.developersam.web.database.Consumer
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonDeserializationContext

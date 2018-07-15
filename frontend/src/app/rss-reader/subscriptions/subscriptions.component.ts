ff9d152709c9638112ad7cee82f3c61a212eb0f9
Sam <sam@developersam.com>
2018-07-15 15:27:29 +0800

    Implemented basic of RSS subscription in frontend

--- frontend/src/app/rss-reader/subscriptions/subscriptions.component.ts
+++ frontend/src/app/rss-reader/subscriptions/subscriptions.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { AlertComponent } from '../../shared/alert/alert.component';
 import { LoadingOverlayService } from '../../shared/overlay/loading-overlay.service';
-import { shortDelay } from '../../shared/util';
+import { asyncRun, shortDelay } from '../../shared/util';
 import { Feed } from '../rss-reader-data';
 import { RssReaderDataService } from '../rss-reader-data.service';
 
@@ -11,6 +12,12 @@ import { RssReaderDataService } from '../rss-reader-data.service';
   styleUrls: ['./subscriptions.component.css']
 })
 export class SubscriptionsComponent implements OnInit {
+
+  /**
+   * The input for RSS Feed URL.
+   * @type {string}
+   */
+  rssFeedUrlInput = '';
 
   constructor(private dataService: RssReaderDataService,
               private loadingService: LoadingOverlayService,
@@ -40,6 +47,35 @@ export class SubscriptionsComponent implements OnInit {
    */
   get subscriptions(): Feed[] {
     return this.dataService.data.subscriptions;
+  }
+
+  /**
+   * Returns whether the add feed button should be disabled to ensure input correctness.
+   *
+   * @returns {boolean} whether the add feed button should be disabled to ensure input correctness.
+   */
+  get isAddFeedButtonDisabled(): boolean {
+    const url = this.rssFeedUrlInput.trim();
+    return url.length === 0 ||
+      this.dataService.data.subscriptions.find(v => v.rssUrl === url) != null;
+  }
+
+  /**
+   * Add a feed.
+   *
+   * @param {KeyboardEvent} event the optional keyboard event.
+   */
+  addFeed(event?: KeyboardEvent): void {
+    if (event && event.code !== 'Enter') {
+      return;
+    }
+    asyncRun(async () => {
+      const ref = this.loadingService.open();
+      const isSuccessful = await this.dataService.addFeed(this.rssFeedUrlInput);
+      const msg = isSuccessful ? 'You have successfully added a feed.' : 'The feed URL is invalid.';
+      ref.close();
+      this.dialog.open(AlertComponent, { data: msg });
+    });
   }
 
 }

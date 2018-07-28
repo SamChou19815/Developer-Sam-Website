4349e66d7ca5a0b4aead42b7a8e3727a1762f31d
Sam <sam@developersam.com>
2018-07-28 15:57:53 +0800

    Make request async to avoid deadline exceeded

--- gae-cron/main.py
+++ gae-cron/main.py
@@ -4,9 +4,8 @@ import urllib2
 
 class HourlyCronPage(webapp2.RequestHandler):
     def get(self):
-        urlfetch.fetch(
-            'https://developersam.com/apis/public/rss_reader/cron', validate_certificate=True)
+        rpc = urlfetch.create_rpc()
+        urlfetch.make_fetch_call(
+            rpc, 'https://developersam.com/apis/public/rss_reader/cron', validate_certificate=True)
 
-app = webapp2.WSGIApplication([
-    ('/hourly', HourlyCronPage),
-], debug=True)
+app = webapp2.WSGIApplication([('/hourly', HourlyCronPage),], debug=True)
